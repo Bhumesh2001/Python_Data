3d5c6a4a9b96e0be3c45fd5f91cbd8cf01e2edb2
bhumesh kewat <96462127+bhumesh789@users.noreply.github.com>
2022-05-05 01:10:07 +0530

    Add files via upload

---
+++ flatten.py
@@ -0,0 +1,28 @@
+# flattern list 
+
+#  l = [1, 2, [3, 4, [5, [[[6]], 7], 8, [9, [10,[12,45,34]]]]]]
+# output = []
+# def call(l):
+#     for i in l:
+#         if type(i) == list:
+#             call(i)
+#         else:
+#             output.append(i)
+
+# print ('The original list: ',l)
+# call(l)
+# print ('The list after removing nesting: ', output)
+
+
+
+# list1 = [1, 2, [3, 4, [5, [[[6]], 7], 8, [9, [10,[12,45,34]]]]]]
+# l2 = []
+# print("orignal list = ",list1)
+# while list1:
+#     r = list1.pop()
+#     if type(r) == list:
+#         list1.extend(r)
+#     else:
+#         l2.append(r)
+# l2.sort()
+# print(l2)
