bdea8167360280c198453fa5cbe6dbd1fea78ed0
bhumesh kewat <96462127+bhumesh789@users.noreply.github.com>
2022-05-05 01:21:44 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,10 @@
+# pattern 
+
+b = int(input("enter your numbers: "))
+for j in range(1,b+1):
+    print(str(j)*j)
+
+
+n = int(input("enter your range: "))
+for i in range(1,n+1):
+    print((10**(i)//9*i))
