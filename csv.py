8cd43b343adc8ae3f799fe6bdf174d37736e380e
bhumesh kewat <96462127+bhumesh789@users.noreply.github.com>
2022-06-30 21:58:12 +0530

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,24 +1,23 @@
-# csv long form:- comma seprated value.
+csv long form:- comma seprated value.
 
-# import csv
-# def create():
-#     with open("details.csv","w") as obj:
-#         fobj = csv.writer(obj)
-#         fobj.write(["roll","name","total_marks"])
-#         while True:
-#             roll = int(input("enter your roll no:  "))
-#             name = input("enter your name: ")
-#             total = int(input("enter your total marks: "))
-#             record = [roll,name,total]
-#             fobj.writerow(record)
-#             gh = input("1. enter more\n 2.exist\n 3.enter your choice")
-#             if gh == "2":
-#                 break
-# # def display():
-# #     with open("details.csv","r") as obj:
-# #         fobj = csv.reader(obj)
-# #         for j in fobj:
-# #             print(j)
-# create()
-# display()
-
+import csv
+def create():
+    with open("details.csv","w") as obj:
+        fobj = csv.writer(obj)
+        fobj.write(["roll","name","total_marks"])
+        while True:
+            roll = int(input("enter your roll no:  "))
+            name = input("enter your name: ")
+            total = int(input("enter your total marks: "))
+            record = [roll,name,total]
+            fobj.writerow(record)
+            gh = input("1. enter more\n 2.exist\n 3.enter your choice")
+            if gh == "2":
+                break
+# def display():
+#     with open("details.csv","r") as obj:
+#         fobj = csv.reader(obj)
+#         for j in fobj:
+#             print(j)
+create()
+display()
